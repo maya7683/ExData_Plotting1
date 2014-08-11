326ca52c3b78be384b663a88cdb6387a434112a2
maya7683 <maya@KAMBP.local>
2014-08-10 17:25:58 -0700

    c1

--- plot3.R
+++ plot3.R
@@ -9,6 +9,6 @@ data$DT <- strptime(paste(data$Date, data$Time), "%d/%m/%Y %H:%M:%S")
 
 data$Global_active_power <- as.numeric(as.character(data$Global_active_power))
 
-png(filename = "plot2.png", width = 480, height = 480)
-plot(data$DT,data$Global_active_power,type="l",xlab="",ylab="Global Active Power (kilowatts)")
-dev.off()
+#png(filename = "plot2.png", width = 480, height = 480)
+#plot(data$DT,data$Global_active_power,type="l",xlab="",ylab="Global Active Power (kilowatts)")
+#dev.off()
